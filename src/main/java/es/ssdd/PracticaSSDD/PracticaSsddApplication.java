08c558c981a734e70bc459b3077cb942efdc255a
Gabriel Sanchez Losa <m318369@alunos.ipb.pt>
2024-03-05 19:49:59 +0100

    Código generado

---
+++ src/main/java/es/ssdd/PracticaSSDD/PracticaSsddApplication.java
@@ -0,0 +1,13 @@
+package es.ssdd.PracticaSSDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSsddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSsddApplication.class, args);
+	}
+
+}
