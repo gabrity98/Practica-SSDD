e4124a3b06a6887319d17c28fe75e75c61d77a02
gabrity98 <g.sanchezl.2020@alumnos.urjc.es>
2024-04-15 23:27:09 +0200

    DataLoader

---
+++ src/main/java/es/ssdd/PracticaSSDD/DataLoader.java
@@ -0,0 +1,24 @@
+package es.ssdd.PracticaSSDD;
+
+import es.ssdd.PracticaSSDD.repositories.PeliculaRepository;
+import es.ssdd.PracticaSSDD.repositories.ReviewRepository;
+import es.ssdd.PracticaSSDD.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public void run(String... args) throws Exception {}
+}
