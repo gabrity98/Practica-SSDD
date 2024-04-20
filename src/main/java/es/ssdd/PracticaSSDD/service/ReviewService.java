50c994288ec5ce08f99838ba861be9ea05df3cce
gabrity98 <g.sanchezl.2020@alumnos.urjc.es>
2024-04-20 19:19:13 +0200

    ReviewService (wip)

--- src/main/java/es/ssdd/PracticaSSDD/service/ReviewService.java
+++ src/main/java/es/ssdd/PracticaSSDD/service/ReviewService.java
@@ -1,45 +1,42 @@
 package es.ssdd.PracticaSSDD.service;
 
 import es.ssdd.PracticaSSDD.entities.Review;
+import es.ssdd.PracticaSSDD.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 @Service
 public class ReviewService {
-    private final Map<Long, Review> reviews = new HashMap<>();
-    private final AtomicLong nextId = new AtomicLong();
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     public Review crearReview(Review review){
         if (review.getAutor() == null || review.getContenido() == null)
             return null;
-        long id = nextId.incrementAndGet();
-        review.setId(id);
-        reviews.put(id,review);
+        reviewRepository.save(review);
         return review;
     }
 
     public Review getReview(Long id){
-        return reviews.get(id);
+        return reviewRepository.findById(id).orElse(null);
     }
 
     public Collection<Review> getAllReviews(){
-        return reviews.values();
+        return reviewRepository.findAll();
     }
 
     public Review actualizarReview(Long id, Review review){
-        if (!reviews.containsKey(id))
+        if (!reviewRepository.existsById(id))
             return null;
         if (review.getAutor() == null || review.getContenido() == null)
             return null;
         review.setId(id);
-        reviews.put(id, review);
+        reviewRepository.save(review);
         return review;
     }
 
     public void eliminarReview(Long id){
-        reviews.remove(id);
+        reviewRepository.deleteById(id);
     }
 }
