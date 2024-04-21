692dcb618e1c205aa1b2dd64b3796a75dc7c1002
gabrity98 <g.sanchezl.2020@alumnos.urjc.es>
2024-04-21 13:11:04 +0200

    ReviewService def

--- src/main/java/es/ssdd/PracticaSSDD/service/ReviewService.java
+++ src/main/java/es/ssdd/PracticaSSDD/service/ReviewService.java
@@ -1,5 +1,6 @@
 package es.ssdd.PracticaSSDD.service;
 
+import es.ssdd.PracticaSSDD.entities.Pelicula;
 import es.ssdd.PracticaSSDD.entities.Review;
 import es.ssdd.PracticaSSDD.repositories.PeliculaRepository;
 import es.ssdd.PracticaSSDD.repositories.ReviewRepository;
@@ -17,8 +18,11 @@ public class ReviewService {
     public Review crearReview(Review review, Long idPelicula){
         if (review.getAutor() == null || review.getContenido() == null)
             return null;
+        Pelicula pelicula = peliculaRepository.findById(idPelicula).orElseThrow(() ->
+                new IllegalArgumentException("La película con id " + idPelicula + " no existe"));
         reviewRepository.save(review);
-        peliculaRepository.getReferenceById(idPelicula).getReviews().add(review);
+        review.setPelicula(pelicula);
+        pelicula.getReviews().add(review);
         return review;
     }
 
@@ -30,18 +34,24 @@ public class ReviewService {
         return reviewRepository.findAll();
     }
 
-    public Review actualizarReview(Long id, Review review, Long idPelicula){
+    public Review actualizarReview(Long id, Review review){
         if (!reviewRepository.existsById(id))
             return null;
         if (review.getAutor() == null || review.getContenido() == null)
             return null;
+        Review reviewVieja = reviewRepository.findById(id).get();
+        Pelicula pelicula = peliculaRepository.findById(reviewVieja.getPelicula().getId()).get();
+        pelicula.getReviews().remove(reviewVieja);
         review.setId(id);
         reviewRepository.save(review);
-        peliculaRepository.getReferenceById(idPelicula).getReviews().add(review);
+        pelicula.getReviews().add(review);
         return review;
     }
 
     public void eliminarReview(Long id){
+        Review review = reviewRepository.findById(id).get();
+        Pelicula pelicula = peliculaRepository.findById(review.getPelicula().getId()).get();
+        pelicula.getReviews().remove(review);
         reviewRepository.deleteById(id);
     }
 }
